dfc1bb4c342b34ba7472b4ddddddb325d59b821f
VoodPekker <voodpeeker@gmail.com>
2023-04-17 18:49:15 +0300

    HW9 v4

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -33,8 +33,6 @@ function onPromiseCreate(e) {
   let inputAmount = Number(amount.value);
 
   for (let i = 1; i <= inputAmount; i += 1) {
-    inputDelay += inputStep;
-
     createPromise(i, inputDelay)
       .then(({ position, delay }) => {
         Notify.success(
@@ -48,6 +46,7 @@ function onPromiseCreate(e) {
           options
         );
       });
+    inputDelay += inputStep;
     e.currentTarget.reset();
   }
 }
